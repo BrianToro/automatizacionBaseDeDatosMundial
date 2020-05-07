3661345b97228256a18f24dbfcc45f3bbacfb9ce
Brian Toro <brianhorrors@hotmail.com>
2020-05-06 19:29:00 -0500

    Automatizacion terminada

---
+++ README.md
@@ -0,0 +1,47 @@
+#Automatizacion Mundial XI - 8
+Este programa hecho en python automatiza la subida
+de registros de jugadores y equipos en la base de datos
+de ejemplo en concreto las tablas de jugadores y la de
+equipos basada en el mundial del 2018 <br/>
+
+numero total de equipos que se registran: 32 <br/>
+numero total de jugadores que se registran: 352
+##Instrucciones:
+* Primero necesitas descargar unas dependencias de python
+con el siguiente comando:
+`pip install pymysql`
+y
+`pip install requests`
+
+* Luego edita el codigo para que tenga las mismas
+credenciales que tu necesitas, por ejemplo el nombre
+de la base de datos y las sentencias SQl, por ejemplo este pedazo
+de codigo debes reemplazar el nombre de la base de la datos
+por la tuya y tu user y password del MYSQL:
+```
+self.connection = pymysql.connect(
+            host='localhost',
+            user='root',
+            password='',
+            db='mundial_seccion8'
+        )
+``` 
+o por ejemplo esta sentencia debes cambiar en esta sentencia
+el nombre tu tabla y el nombre los campos:
+
+``` 
+sql = "INSERT INTO equipos (idEquipo, nombreEquipo, grupo) 
+VALUES ('{}', '{}', '{}');".format(id_equipo, nombre_equipo, grupo)
+```
+* Ejecuta el xampp y enciende el proceso de MySql 
+
+* Luego debes ejecutar el archivo de python llamado main.py
+(es el unico archivo que hay xd)
+
+* Al final verifica los registros que se crearon en tu base de datos
+con el siguiente comando en MySql Workbench o PhpMyAdmin
+
+`SELECT * FROM AquiVaElNombreDeTuTablaDeJugadoresOEquipo;`
+
+Cualquier duda, problema o error pueden escribirme para guiarlos
+con esto
---
+++ main.py
@@ -0,0 +1,92 @@
+import pymysql
+import requests
+
+#urls de API'S que traen la informacion
+URL_JUGADORES = 'https://world-cup-json-2018.herokuapp.com/matches'
+URL_EQUIPOS = 'https://world-cup-json-2018.herokuapp.com/teams'
+
+#Clase de la base de datos para manipularla
+class Database:
+    def __init__(self):
+        self.connection = pymysql.connect(
+            host='localhost',
+            user='root',
+            password='',
+            db='mundial_seccion8'
+        )
+
+        self.cursor = self.connection.cursor()
+        print('Conexion establecida correctamente')
+    def select_sede(self):
+        sql = 'SELECT * FROM sedes;'
+        try:
+            self.cursor.execute(sql)
+            sedes = self.cursor.fetchall()
+            print(sedes)
+        except Exception as err:
+            raise
+    def select_equipo(self, nombre_equipo):
+        sql = "SELECT * FROM equipos WHERE nombreEquipo = '{}';".format(nombre_equipo)
+        try:
+            self.cursor.execute(sql)
+            equipo = self.cursor.fetchone()
+            return equipo
+        except Exception as err:
+            raise
+
+    def insertar_equipos(self, id_equipo, nombre_equipo, grupo):
+        sql = "INSERT INTO equipos (idEquipo, nombreEquipo, grupo) VALUES ('{}', '{}', '{}');".format(id_equipo, nombre_equipo, grupo)
+        try:
+            self.cursor.execute(sql)
+            self.connection.commit()
+        except Exception as err:
+            raise
+
+    def insert_jugadores(self, id_jugador, nombre_jugador, posicion, id_equipo):
+        sql = "INSERT INTO jugadores (idJugadore, nombreJugador, idEquipo, posicion) VALUES ('{}', '{}', '{}', '{}')".format(id_jugador, nombre_jugador, id_equipo, posicion)
+        try:
+            self.cursor.execute(sql)
+            self.connection.commit()
+        except Exception as err:
+            raise
+
+database = Database()
+#database.select_sede()
+#database.select_equipo("Panama")
+
+def obtener_equipos(url, database):
+    response = requests.get(url)
+    if response.status_code == 200:
+        response_json = response.json()
+        for i in range(len(response_json)):
+            #print(response_json[i])
+            id_equipo = response_json[i]['id']
+            nombre_equipo = response_json[i]['country']
+            grupo = response_json[i]['group_letter']
+            database.insertar_equipos(id_equipo, nombre_equipo, grupo)
+
+def traer_todo(url, database):
+    countrys = []
+    response = requests.get(url)
+    if response.status_code == 200:
+        response_json = response.json()
+        for i in range(len(response_json)):
+            datos = response_json[i]
+            datos_concretos = datos['home_team_statistics']
+            extra_id = 0
+            if datos_concretos['country'] not in countrys:
+                current_equipo = database.select_equipo(datos_concretos['country'])
+                countrys.append(datos_concretos['country'])
+                for x in range(len(datos_concretos['starting_eleven'])):
+                    currentJugador = datos_concretos['starting_eleven'][x]
+                    id_jugador = int(str(current_equipo[0]) + str(currentJugador['shirt_number']) + str(extra_id))
+                    database.insert_jugadores(id_jugador, str(currentJugador['name']), str(currentJugador['position']), int(current_equipo[0]))
+                    extra_id += 1
+            else:
+                continue
+
+    else:
+        print('Error con el API')
+
+#obtener_equipos(URL_EQUIPOS, database)
+traer_todo(URL_JUGADORES, database)
